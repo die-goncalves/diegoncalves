aa8c71a9cad33dbd084b6a73515d7d6b94ccf4d4
die-goncalves <die.goncalves1990@gmail.com>
2023-09-19 16:02:32 -0300

    style: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 const config: Config = {
+  darkMode: ['class', '[data-theme="dark"]'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -13,6 +14,26 @@ const config: Config = {
         serif: ['var(--font-erode)', ...defaultTheme.fontFamily.serif],
         sans: ['var(--font-tasa-explorer)', ...defaultTheme.fontFamily.sans],
         mono: ['var(--font-iosevka)', ...defaultTheme.fontFamily.mono]
+      },
+      colors: {
+        transparent: '#00000000',
+        brand: '#5BC5AC',
+        selection: '#78a3ffdd',
+        mark: '#99ffe7',
+        white: '#FFFFFF',
+        'white/8': '#FFFFFF14',
+        'white/16': '#FFFFFF29',
+        'white/92': '#FFFFFFEB',
+        black: '#000000',
+        'black/8': '#00000014',
+        'black/16': '#00000029',
+        'black/92': '#000000EB',
+        light: '#F7F7F7',
+        'light/link-primary': '#1E40AF',
+        'light/link-secondary': '#172554',
+        dark: '#282729',
+        'dark/link-primary': '#93C5FD',
+        'dark/link-secondary': '#A5B5C6'
       }
     }
   },
