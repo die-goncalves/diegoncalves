450787c0e3dcd0525e783f62d52f279e52d5ba05
die-goncalves <die.goncalves1990@gmail.com>
2023-09-19 11:02:40 -0300

    feat: add erode, iosevka and tasa-explorer fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,16 @@
+import localFont from 'next/font/local'
+
+export const tasa_explorer = localFont({
+  src: './font-tasa-explorer.woff2',
+  variable: '--font-tasa-explorer'
+})
+
+export const erode = localFont({
+  src: './font-erode.ttf',
+  variable: '--font-erode'
+})
+
+export const iosevka = localFont({
+  src: './font-iosevka-slab-extended.ttf',
+  variable: '--font-iosevka'
+})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import clsx from 'clsx'
+import { erode, iosevka, tasa_explorer } from './fonts'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +14,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body
+        className={clsx(
+          `${tasa_explorer.variable} ${erode.variable} ${iosevka.variable}`
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
