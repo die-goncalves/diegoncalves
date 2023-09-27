3edcc333b891f75849008d004d846c646d34a317
die-goncalves <die.goncalves1990@gmail.com>
2023-09-27 09:14:14 -0300

    feat(src/components/hyperlink.tsx): add new classes

--- src/components/hyperlink.tsx
+++ src/components/hyperlink.tsx
@@ -7,14 +7,20 @@ type LinkType = ComponentProps<typeof Link>
 interface HyperlinkProps extends LinkType {
   external?: boolean
 }
-export function Hyperlink({ external, children, ...props }: HyperlinkProps) {
+export function Hyperlink({
+  external,
+  children,
+  className,
+  ...props
+}: HyperlinkProps) {
   return (
     <Link
       className={clsx(
         'underline decoration-solid decoration-auto underline-offset-2',
         'outline-2 outline-offset-2 outline-black/92 focus-visible:outline dark:outline-white/92',
         'font-medium text-light/link-primary visited:text-light/link-secondary dark:text-dark/link-primary dark:visited:text-dark/link-secondary',
-        'hover:no-underline focus-visible:no-underline'
+        'hover:no-underline focus-visible:no-underline',
+        className
       )}
       {...(external && {
         target: '_blank'
