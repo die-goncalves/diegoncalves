aaa8a1756eb2034cb8c18bae260f1b2989702a28
die-goncalves <die.goncalves1990@gmail.com>
2023-10-09 14:20:22 -0300

    feat(src/app/page.tsx): add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,175 @@
+import { Metadata } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
+import request, { gql } from 'graphql-request'
+import clsx from 'clsx'
+import { formattedDate } from '@/utils/date'
+import { urlToBaseURL } from '@/utils/toBase64'
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'Página inicial | Diego Gonçalves'
+}
+
+async function getLastPosts() {
+  const endpoint = process.env.CMS_HYGRAPH ?? ''
+
+  const query = gql`
+    query GetLastPosts {
+      posts(stage: PUBLISHED, last: 2) {
+        id
+        slug
+        title
+        createdAt
+        updatedAt
+      }
+    }
+  `
+
+  const data: {
+    posts: {
+      id: string
+      slug: string
+      title: string
+      createdAt: string
+      updatedAt: string
+    }[]
+  } = await request(endpoint, query)
+
+  return {
+    oldPost: data.posts[0],
+    newPost: data.posts[1]
+  }
+}
+
+async function getAvatar() {
+  const endpoint = process.env.CMS_HYGRAPH ?? ''
+
+  const query = gql`
+    query GetAvatar {
+      assets(where: { fileName: "avatar.png" }) {
+        id
+        url
+        blurDataURL: url(
+          transformation: {
+            image: { resize: { height: 10, width: 10, fit: clip } }
+          }
+        )
+        mimeType
+      }
+    }
+  `
+
+  const data: {
+    assets: {
+      id: string
+      url: string
+      blurDataURL: string
+      mimeType: string
+    }[]
+  } = await request(endpoint, query)
+
+  return {
+    url: data.assets[0].url,
+    blurDataURL: await urlToBaseURL(
+      data.assets[0].blurDataURL,
+      data.assets[0].mimeType
+    )
+  }
+}
+
+export default async function Home() {
+  const { newPost, oldPost } = await getLastPosts()
+  const { url, blurDataURL } = await getAvatar()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <article className="mx-auto min-h-[calc(100vh-(88px+139px))] max-w-4xl px-4 pt-8 sm:min-h-[calc(100vh-(88px+88px))] sm:px-8">
+        <div className="my-auto flex items-center justify-center">
+          <div className="flex flex-col items-center gap-8 md:flex-row">
+            <div className="relative aspect-video h-[300px] w-[225px]">
+              <Image
+                src={url}
+                alt=""
+                placeholder="blur"
+                blurDataURL={blurDataURL}
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="flex flex-col gap-4 text-center md:text-start">
+              <span>Oi, eu sou</span>
+              <h1 className="mx-auto max-w-min text-3xl font-bold md:mx-0">
+                Diego Gonçalves
+              </h1>
+              <span className="max-w-md">
+                Crio e compartilho minhas aplicações web que integram design,
+                codificação, interface/experiência do usuário e acessibilidade.
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {(newPost || oldPost) && (
+          <div className="mx-auto mt-8 flex w-full flex-col gap-2 md:w-[705px]">
+            <div className="border-gradient box-gradient"></div>
+            <h2 className="text-base font-semibold">
+              {newPost && oldPost ? 'Últimas públicações' : 'Última públicação'}
+            </h2>
+            <nav>
+              <ul
+                className={clsx(
+                  'grid gap-4 md:grid-cols-2 md:grid-rows-1',
+                  !(newPost && oldPost) ? 'grid-rows-1' : 'grid-rows-2'
+                )}
+              >
+                {newPost && (
+                  <li className="group flex h-fit flex-1">
+                    <Link
+                      href={`${process.env.DOMAIN}/artigo/${newPost.slug}`}
+                      className={clsx(
+                        'border-gradient group box-border grid flex-1 items-start overflow-visible p-4 [grid-template-rows:min-content_min-content]',
+                        'outline-2 outline-offset-2 outline-black/92 focus-visible:outline dark:outline-white/92',
+                        'duration-300 ease-out-quart [transition-property:background]',
+                        'focus-visible:bg-black/8 group-hover:bg-black/8 group-active:bg-black/16 dark:focus-visible:bg-white/8 dark:group-hover:bg-white/8 dark:group-active:bg-white/16'
+                      )}
+                      aria-label={`Ir para a publicação anterior: `}
+                    >
+                      <p className="line-clamp-2 font-medium">
+                        {newPost.title}
+                      </p>
+                      <p className="text-black/80 dark:text-white/80">
+                        {formattedDate({ date: newPost.updatedAt })}
+                      </p>
+                    </Link>
+                  </li>
+                )}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 font-sans text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] font-serif text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 font-sans text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] font-serif text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 font-sans text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] font-serif text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 font-sans text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] font-serif text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                {oldPost && (
+                  <li className="group flex h-fit flex-1 md:col-start-2">
+                    <Link
+                      href={`${process.env.DOMAIN}/artigo/${oldPost.slug}`}
+                      className={clsx(
+                        'group grid flex-1 items-start p-4 [grid-template-rows:min-content_min-content]',
+                        'outline-2 outline-offset-2 outline-black/92 focus-visible:outline dark:outline-white/92',
+                        'duration-300 ease-out-quart [transition-property:background]',
+                        'focus-visible:bg-black/8 group-hover:bg-black/8 group-active:bg-black/16 dark:focus-visible:bg-white/8 dark:group-hover:bg-white/8 dark:group-active:bg-white/16'
+                      )}
+                      aria-label={`Ir para a próxima publicação:`}
+                    >
+                      <p className="line-clamp-2 font-medium">
+                        {oldPost.title}
+                      </p>
+                      <p className="text-black/80 dark:text-white/80">
+                        {formattedDate({ date: oldPost.updatedAt })}
+                      </p>
+                    </Link>
+                  </li>
+                )}
+              </ul>
+            </nav>
+          </div>
+        )}
+      </article>
     </main>
   )
 }
