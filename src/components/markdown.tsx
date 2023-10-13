d7c78ce47bb853ca48612a1835649a8eb246f34e
die-goncalves <die.goncalves1990@gmail.com>
2023-10-13 11:57:17 -0300

    fix(src/components/markdown.tsx): picture component was being considered as server component
    Error reproduction: comment/uncomment 'code' of the components object and save file. A static page
    that contains an image in the markdown body will indicate: Error: async/await is not yet supported
    in Client Components, only Server Components. This error is often caused by accidentally adding
    `'use client'` to a module that was originally written for the server.

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -4,8 +4,15 @@ import { HTMLAttributes } from 'react'
 import { MDXRemote } from 'next-mdx-remote'
 import { CH } from '@code-hike/mdx/components'
 import { Hyperlink } from './hyperlink'
-import { IPicture, Picture } from './picture'
 import { Player, IPlayer } from './player/player'
+import Image, { ImageProps } from 'next/image'
+import { CameraIcon } from './icons/camera'
+
+type IPicture = ImageProps & {
+  photographer?: string
+  contact?: string
+  fallback?: string
+}
 
 const components = {
   CH,
@@ -53,10 +60,47 @@ const components = {
       {...props}
     />
   ),
-  ImageComponent: (props: IPicture) => {
+  ImageComponent: ({
+    src,
+    alt,
+    fallback,
+    photographer,
+    contact,
+    ...rest
+  }: IPicture) => {
     return (
       <div className="mx-auto mb-4 max-w-4xl md:px-8">
-        <Picture {...props} />
+        <figure>
+          <div className="relative aspect-video w-full">
+            <Image
+              src={src}
+              alt={alt}
+              fill
+              className="object-cover"
+              {...(fallback && {
+                placeholder: 'blur',
+                blurDataURL: fallback
+              })}
+              {...rest}
+            />
+          </div>
+          {(alt || photographer) && (
+            <figcaption className="flex flex-wrap items-start px-4 py-1 md:px-0">
+              <CameraIcon className="mr-2 hidden h-6 w-6 flex-none fill-black/16 align-text-top dark:fill-white/16 sm:inline" />
+              <p className="text-sm">{alt}</p>
+              {alt && photographer && <p className="text-sm">&nbsp;--&nbsp;</p>}
+              {photographer && <p className="text-sm">Foto:&nbsp;</p>}
+              {photographer &&
+                (contact ? (
+                  <Hyperlink external href={contact} className="text-sm">
+                    {photographer}
+                  </Hyperlink>
+                ) : (
+                  <p className="text-sm">{photographer}</p>
+                ))}
+            </figcaption>
+          )}
+        </figure>
       </div>
     )
   },
