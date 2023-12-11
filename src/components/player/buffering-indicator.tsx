bd696bb9bd89a102bf310c70262efb3747c8c7c9
die-goncalves <die.goncalves1990@gmail.com>
2023-12-11 10:25:11 -0300

    style(src/components/player/buffering-indicator.tsx): put the load indicator on top of the controls

--- src/components/player/buffering-indicator.tsx
+++ src/components/player/buffering-indicator.tsx
@@ -2,7 +2,7 @@ import { ProgressActivityIcon } from '../icons/progress-activity'
 
 export function BufferingIndicator() {
   return (
-    <div className="vds-buffering-indicator">
+    <div className="vds-buffering-indicator z-50">
       <ProgressActivityIcon className="vds-buffering-icon fill-white/92" />
     </div>
   )
